0076d56f41bc38c4d5adc5a1a712e5f4e7a18b05
Dan <inb3zna@gmail.com>
2024-01-11 18:56:06 +0200

    add application.properties DB info, ModelMapper and exception handler basic structure

---
+++ src/main/java/com/dan/boardgame_cafe/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.dan.boardgame_cafe.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    private ResponseEntity<Object> getResponse(RuntimeException e, HttpStatus httpStatus) {
+        Map<String, Object> result = new HashMap<>();
+        result.put("message", e.getMessage());
+        return new ResponseEntity<>(result, httpStatus);
+    }
+}
